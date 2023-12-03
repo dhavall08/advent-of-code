8ab7b80992e560d16551dcff46843548911ce3fb
Dhaval Laiya <dhavalbl01@gmail.com>
2023-12-04 00:10:07 +0530

    1.1

---
+++ day1/1/1.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+/* -------------------- getOutput - start ------------------------- */
+function getOutput(data) {
+  const lines = data.split("\r\n");
+
+  const digitsRegex = /[0-9]/g;
+
+  const total = lines.reduce((sum, line) => {
+    // get digits
+    const digits = line.match(digitsRegex);
+
+    let calibrationValue;
+
+    if (digits.length === 1) {
+      calibrationValue = String(digits[0]) + String(digits[0]);
+    }
+
+    calibrationValue = String(digits.at(0)) + String(digits.at(-1));
+
+    return sum + +calibrationValue;
+  }, 0);
+
+  return total;
+}
+/* -------------------- getOutput - end ------------------------- */
+
+const input = fs.readFileSync(
+  path.resolve(__dirname, "./1_input.txt"),
+  "utf-8"
+);
+
+const output = getOutput(input);
+console.log("Answer:", output);
