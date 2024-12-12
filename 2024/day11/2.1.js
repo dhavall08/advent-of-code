d12f08e8562debc76df4c69a988f22d05dde9095
Dhaval Laiya <dhavalbl01@gmail.com>
2024-12-12 21:14:20 +0530

    wip: 11.2

---
+++ 2024/day11/2.1.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs.readFileSync(path.resolve(__dirname, "./input.txt"), "utf-8");
+const charArr = input.split(" ").map((a) => +a);
+const sum = charArr.reduce((sum, a) => {
+  return sum + getOutput([a]);
+}, 0);
+// const output = getOutput(input.split(" ").map((a) => +a));
+console.log("output:", sum);
+
+function getOutput(input) {
+  input;
+
+  let curr = input;
+  for (let i = 0; i < 75; i++) {
+    curr = getStones(curr);
+  }
+
+  return curr.length;
+}
+
+function getStones(array) {
+  let newArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let a = array[i];
+
+    if (a === 0) {
+      newArr.push(1);
+      continue;
+    }
+
+    if (String(a).length % 2 === 0) {
+      newArr.push(Number(String(a).slice(0, String(a).length / 2)));
+      newArr.push(Number(String(a).slice(String(a).length / 2)));
+      continue;
+    }
+
+    newArr.push(a * 2024);
+  }
+
+  return newArr;
+}
